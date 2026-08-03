49aaf28ece81265b4eecbd32425774c58ed45788
Abhijeet Kumar <abhijeetnandani@gmail.com>
2026-08-03 22:43:32 +0530

    Fix MD5 security finding

---
+++ src/main/java/securitydemo/VulnerableExamples.java
@@ -0,0 +1,23 @@
+package com.securedevops.employeemanagement.securitydemo;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class VulnerableExamples {
+
+    public void vulnerableSqlQuery(String employeeId) {
+
+        String query = "SELECT * FROM employee WHERE id = '" + employeeId + "'";
+
+        System.out.println(query);
+    }
+
+    public void weakHash(String input) throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+        byte[] hash = md.digest(input.getBytes());
+
+        System.out.println(hash);
+    }
+}
